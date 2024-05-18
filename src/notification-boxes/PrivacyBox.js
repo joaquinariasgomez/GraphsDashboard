467b538ea296d9bd08439a2c9256b8d6b7eeb1e0
José Joaquín Arias Gómez-Calcerrada <joaquinariasgomezcal@gmail.com>
2024-05-18 11:43:20 +0100

    Things

--- src/notification-boxes/PrivacyBox.js
+++ src/notification-boxes/PrivacyBox.js
@@ -17,8 +17,9 @@ export default function PrivacyBox() {
         <div className='privacybox__backdrop' onClick={closeBox}>
             <div className="privacybox__container" onClick={e => {e.stopPropagation(); }}>
                 <div className="privacybox__text__row">
-                    <h1>To fill</h1>
-                    <p>- To fill</p>
+                    <h1>Privacy Policy</h1>
+                    <p>Last updated: 18/05/2024</p>
+                    <p>Last updated: 18/05/2024</p>
                 </div>
                 <div className="privacybox__options__row">
                     <button className="privacybox__cancelbutton" onClick={function() {
