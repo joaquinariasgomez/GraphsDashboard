6126c9e8940f91050db1746719803a82d8745564
José Joaquín Arias Gómez-Calcerrada <joaquinariasgomezcal@gmail.com>
2024-05-19 08:33:41 +0100

    Changed redirect uri for prod

--- src/LoginBox.js
+++ src/LoginBox.js
@@ -2,7 +2,8 @@
 
 export default function LoginBox({  }) {
 
-    const authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FGraphsDashboard";
+    //const local_authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FGraphsDashboard";
+    const authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fjoaquinariasgomez.github.io%2FGraphsDashboard%2F"
 
     return (
         <a className="login__button" href={authorization_url}>
