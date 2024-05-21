9cf13f727c44214d1c61c6698bce0a8370bd74e6
José Joaquín Arias Gómez-Calcerrada <joaquinariasgomezcal@gmail.com>
2024-05-21 08:18:01 +0100

    TEsting

--- src/LoginBox.js
+++ src/LoginBox.js
@@ -3,7 +3,7 @@
 export default function LoginBox({  }) {
 
     //const local_authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FGraphsDashboard";
-    const authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fjoaquinariasgomez.github.io%2FGraphsDashboard%2F"
+    const authorization_url = "https://api.notion.com/v1/oauth/authorize?client_id=7b3518ee-dfde-4748-b007-7cd1c3405e0d&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fjoaquinariasgomez.github.io%2FGraphsDashboard";
 
     return (
         <a className="login__button" href={authorization_url}>
